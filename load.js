b0b3e103f2e86d38ef2b7154843bd727cd837283
Ryow <64392776+ryow1003@users.noreply.github.com>
2021-09-30 18:54:13 +0900

    Add files via upload

---
+++ load.js
@@ -0,0 +1,5 @@
+window.onload = function() {
+  setTimeout(function() {
+    $('.load').fadeOut(3000);
+  }, 3000);
+}
---
+++ scroll.js
@@ -0,0 +1,30 @@
+$(function(){
+  $('a[href^="#about"]').click(function(){
+    var speed = 800;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+});
+$(function(){
+  $('a[href^="#access"]').click(function(){
+    var speed = 800;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+});
+$(function(){
+  $('a[href^="#top"]').click(function(){
+    var speed = 800;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+});
